d66c6fd610dec8a033c5583e4beeff583eab7d2a
suripoori <suraj.srinivasa@gmail.com>
2015-05-16 12:26:58 -0400

    Renames all files which have numbers in their names to not having numbers in their names

--- prank/prank.py
+++ prank/prank.py
@@ -1,9 +1,16 @@
 __author__ = 'Suraj'
 
 import os
+import re
 
 def rename_files():
     file_list = os.listdir(r"C:\Users\Suraj\Downloads\prank\prank")
-    print (file_list)
-
+    for file in file_list:
+        try:
+            file = "C:\\Users\\Suraj\\Downloads\\prank\\prank\\" + file
+            new_file_name = re.sub('[0123456789]','',file)
+            #print (new_file_name)
+            os.rename(file,new_file_name)
+        except FileNotFoundError:
+            print("Could not find file %s" % file)
 rename_files()
